18ba4c2ba555393bf32ad8986c7722c1b67d8877
Jewelsnel <jewelle1@live.nl>
2019-10-17 15:54:48 +0200

    Added: Tetris blovk builder base1  - pass level 1, 1 tetric block

---
+++ Show Me prototype 1 - Tetris bridge builder/Assets/Scripts/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject mainCol;
+
+    private main_collider colliderScript;
+
+    public GameObject player;
+    public Rigidbody2D playerRB;
+    public float playerSpeed;
+    private float playerStop = 0;
+    public float playerTimer = 2;
+
+    public Camera mainCamera;
+    private float camY;
+    private float camZ;
+
+    public bool camCanMove = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliderScript = mainCol.gameObject.GetComponent<main_collider>();
+        playerRB = player.gameObject.GetComponent<Rigidbody2D>();
+        mainCamera = mainCamera.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        if (colliderScript.hasCollided == true)
+        {
+            
+            playerRB.transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
+            camCanMove = true;
+            playerTimer -= Time.deltaTime;
+            if (playerTimer < -0)
+            {
+                playerSpeed = playerStop;
+                
+                //playerRB.transform.Translate(Vector2.right * 0 * Time.deltaTime);
+            }
+            colliderScript.hasCollided = false;
+            
+            
+
+        }
+
+        if (camCanMove)
+        {
+            mainCamera.transform.position = mainCamera.transform.position + new Vector3(11f, 0f, 0f) * Time.deltaTime;
+            camCanMove = false;
+        }
+    }
+}
---
+++ Show Me prototype 1 - Tetris bridge builder/Assets/Scripts/tetris_block.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tetris_block : MonoBehaviour
+{
+    Collider2D col;
+    public float newXposition;
+    public float newYposition;
+    public float speed;
+    public bool isTouched;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+                if (col == touchedCollider && isTouched == false)
+                {
+                    isTouched = true;
+                }
+            }
+
+            /*if (touch.phase == TouchPhase.Moved)
+            {
+                if (isTouched)
+                {
+                    transform.position = new Vector2(newXposition, newYposition);
+                }
+            }*/
+
+
+            if (touch.phase == TouchPhase.Ended)
+            {
+                if (isTouched)
+                {
+                 transform.position = transform.position + new Vector3(0f,-3.74f,0f);
+                    
+                }
+            }
+        }
+    }
+}
